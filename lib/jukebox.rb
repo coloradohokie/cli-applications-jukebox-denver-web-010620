1c0b9116b949a64062594cc587a84ff497afe967
Michael Newman <coloradohokie@gmail.com>
2019-12-27 20:42:46 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,11 @@
-# Add your code here
+
+def help
+  puts "I accept the following commands:"
+  puts " - help : displays the help message"
+  puts " - list : displays a list of songs you can play"
+  puts " - exit : exits this program"
+end
+
+def list
+  i=0
+  while i < array.length do
