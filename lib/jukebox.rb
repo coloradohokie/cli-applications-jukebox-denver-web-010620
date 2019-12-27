f73d82b991212d883adbdfbd8410dd03fff93cb7
Michael Newman <coloradohokie@gmail.com>
2019-12-27 20:59:18 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -6,6 +6,31 @@ def help
   puts " - exit : exits this program"
 end
 
-def list
-  i=0
-  while i < array.length do
+def list(song_list)
+  song_list.each_with_index do |v,i|
+    puts "#{i+1}. #{v}"
+  end
+end
+
+def play(song_list)
+  puts "Please enter a song name or number:"
+  input = gets.strip
+  song_list.each_with_index do |v,i|
+    if input == (v || i)
+      puts "Playing #{v}"
+      match_found = true
+    end #if
+  end #songlist.each
+  if !match_found
+    puts "Invalid input, please try again"
+    match_found = nil
+  end #if
+end #method
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+  
+  
+    
